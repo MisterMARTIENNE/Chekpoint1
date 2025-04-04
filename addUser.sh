d57762518b1a17dd1fbb3d7c8b08c2799870290a
Eric MARTIENNE <ericmartienne@gmail.com>
2025-04-04 12:10:43 +0200

    addUser.sh
    Erreur de script que je ne comprends pas

---
+++ addUser.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#il faut prendre les arguments (en nombre n  et noms)
+
+#Si n=0 afficher " Il manque...."
+
+if [ -n $# ]
+
+then
+    #sinon faire n fois la boucle pour les n noms
+    for user in $@
+    do
+    #Verifier que le nom n'existe pas 
+        if id "$user" &> /dev/null
+        then
+            echo "L'utilisateur $user existe déja"
+        else
+            sudo adduser "$user"
+                #verifier que le compte est crée
+                if [ cat /etc/password | grep "$user" ]
+                then
+                    echo "L'utilisateur $word est crée"
+                #si non message d'erreur
+                else
+                    echo "Erreur à la création de l'utilisateur $user"
+else
+
+echo "Il manque les noms d'utilisateurs en argument - fin de sript"
+
+fi
+
+exit 0
